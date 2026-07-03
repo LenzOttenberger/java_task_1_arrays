d684919d5d2c0872d4381c7363b601c236f34097
LenzOttenberger <Kirill.Drozd15@gmail.com>
2026-07-03 13:20:43 +0300

    feat(DoubleArrayWrapper): create DoubleArrayWrapper class and it methods

--- src/main/java/org/kiryl/arrays/entity/DoubleArrayWrapper.java
+++ src/main/java/org/kiryl/arrays/entity/DoubleArrayWrapper.java
@@ -1,3 +1,12 @@
 package org.kiryl.arrays.entity;
 
-public class DoubleArrayWrapper {}
+public class DoubleArrayWrapper extends ArrayWrapper<Double> {
+  public DoubleArrayWrapper(Double[] elements) {
+    super(elements);
+  }
+
+  @Override
+  public Double getElement(int index) {
+    return elements[index];
+  }
+}
