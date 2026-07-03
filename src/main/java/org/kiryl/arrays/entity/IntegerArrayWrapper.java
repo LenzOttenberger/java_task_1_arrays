8c59a527708d5a28bbfac558f53fded7b33b7050
LenzOttenberger <Kirill.Drozd15@gmail.com>
2026-07-03 13:12:24 +0300

    feat(IntegerArrayWrapper): create IntegerArrayWrapper class and it methods

--- src/main/java/org/kiryl/arrays/entity/IntegerArrayWrapper.java
+++ src/main/java/org/kiryl/arrays/entity/IntegerArrayWrapper.java
@@ -1,3 +1,12 @@
 package org.kiryl.arrays.entity;
 
-public class IntegerArrayWrapper {}
+public class IntegerArrayWrapper extends ArrayWrapper<Integer> {
+  public IntegerArrayWrapper(Integer[] elements) {
+    super(elements);
+  }
+
+  @Override
+  public Integer getElement(int index) {
+    return elements[index];
+  }
+}
